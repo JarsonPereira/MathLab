bc4a2db5b7cfc447857171c77dccecdf3b8f9719
Jarson Pereira <jarson.pereira@semantix.com.br>
2021-07-29 13:48:30 -0300

    add gitignore

--- MathLab.Console/Program.cs
+++ MathLab.Console/Program.cs
@@ -15,7 +15,7 @@ namespace MathLab
             MathContext mathContext = new MathContext(new DividingNumberStrategy());
             var divisors = string.Join(" ", mathContext.GetNumbers(number).Select(x => x.ToString()).ToArray());
             Console.WriteLine("Dividores: "+divisors);
-
+              
             MathContext primeContext = new MathContext(new PrimeNumberStrategy());
             var primes = string.Join(" ", primeContext.GetNumbers(number).Select(x => x.ToString()).OrderBy(x=>x).ToArray());
             Console.WriteLine("Primos: " + primes);
